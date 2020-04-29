a9c34f5e60ab2c19b6bbedc4e557446a52222deb
aslamraff <56871910+aslamraff@users.noreply.github.com>
2020-04-29 10:50:47 +0700

    CRUD Company, CRUD Interview

---
+++ Project_IAP/ViewModels/InterviewVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_IAP.ViewModels
+{
+    public class InterviewVM
+    {
+        public int Id { get; set; }
+        public int CompanyId { get; set; }
+        public string CompanyName { get; set; }
+        public string Title { get; set; }
+        public string Division { get; set; }
+        public string JobDesk { get; set; }
+        public DateTime InterviewDate { get; set; }
+        public string Address { get; set; }
+        public string Description { get; set; }
+    }
+}
